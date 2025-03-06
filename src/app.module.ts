15f0658e423412034c168a3e87cb1767171c2283
mhdiree <mhdiree@sookmyung.ac.kr>
2025-03-06 13:26:59 +0900

    MySQL 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigProjectModule } from './config-project/config-project.module';
-import { MysqlSequelizeModule } from './mysql_sequelize/mysql_sequelize.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'entity/user.model';
 
 @Module({
-  imports: [AuthModule, UserModule, ConfigProjectModule, MysqlSequelizeModule],
+  imports: [AuthModule, UserModule, ConfigProjectModule,
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV == 'dev' ? '.env.dev' : '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWD,
+      database: process.env.DB_DATABASE,
+      entities: [ User ],
+      synchronize: true,
+      logging: true,
+    }),
+   ],
   controllers: [AppController],
   providers: [AppService],
 })
