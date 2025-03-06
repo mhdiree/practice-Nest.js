fb7ad9e79e314c54f9abf8e4d8eb061fc607fe04
mhdiree <mhdiree@sookmyung.ac.kr>
2025-03-06 17:13:46 +0900

    db연동

---
+++ src/user/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+    BeforeInsert,
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+  } from 'typeorm';
+  
+  import * as bcrypt from 'bcrypt';
+  
+  @Entity({ name: 'user' })
+  export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    email: string;
+  
+    @Column()
+    password: string;
+  
+    @Column()
+    nickname: string;
+  
+    @CreateDateColumn()
+    created_at: Date;
+  
+    @BeforeInsert()
+    private beforeInsert() {
+      this.password = bcrypt.hashSync(this.password, 10);
+    }
+  }
