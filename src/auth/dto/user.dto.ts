2c4379e30dea829dc23e954a14d3a854c629fe2c
mhdiree <mhdiree@sookmyung.ac.kr>
2025-03-07 15:46:07 +0900

    swagger추가, 응답형식 수정

--- src/auth/dto/user.dto.ts
+++ src/auth/dto/user.dto.ts
@@ -1,10 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, MaxLength } from "class-validator";
 
+
 export class UserDTO {
+    @ApiProperty()
     @IsString()
     @MaxLength(10)
     username: string;
 
+    @ApiProperty()
     @IsString()
     @MaxLength(20)
     password: string;
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const config = new DocumentBuilder()
+    .setTitle('Nest.js 실습')
+    .setDescription('api 연습 중')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   const port = configService.get("PORT") || '3000';
 
   app.enableCors({
