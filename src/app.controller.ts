7da91ae7b4d7e0921fb0c9c7b9dfe555e1ef9dba
mhdiree <mhdiree@sookmyung.ac.kr>
2025-03-10 15:27:53 +0900

    중복응답 오류 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  redirectToStatic(@Res() res: Response) {
-    res.redirect('/index.html');  // 정적 파일이 있는 경로로 리디렉션
+  Hello(){
+    return "Hello";
   }
 }
