92ecbe46a28ae0b83d96640f0555d65e5faaafa8
mhdiree <mhdiree@sookmyung.ac.kr>
2025-03-11 13:42:32 +0900

    socket기능 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { EventsModule } from './events/events.module';
 import { EventsGateway } from './events/events.gateway';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path'; 
+import { socketGuard } from './auth/guard/socket-token.guard';
 
 @Module({
   imports: [
@@ -45,6 +46,6 @@ import { join } from 'path';
     EventsModule,
    ],
   controllers: [AppController, AuthController],
-  providers: [AppService, AuthService, UserRepository, JwtStrategy, /*EventsGateway*/ ],
+  providers: [AppService, AuthService, UserRepository,],
 })
 export class AppModule {}
